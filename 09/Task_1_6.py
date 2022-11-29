6be5c991b5b3254c26a11d1a2b7471417b756e8f
Albert Blarer <ablarer@gmx.ch>
2022-11-29 14:45:11 +0100

    Lab 09. Finished Task_1_6

--- 09/Task_1_6.py
+++ 09/Task_1_6.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+from fontTools.merge import cmap
+from matplotlib import cm
 import numpy as np
+import scipy.integrate as integrate
 
 c = 1
 L = 1
@@ -17,3 +21,33 @@ nt = len(t)
 
 u = np.zeros((nx, nt))
 a = np.zeros(nmax)
+b = np.zeros(nmax)
+
+
+#define functions (input can be vectors)
+def f(xi): return (xi/L)*(1.-xi/L)
+def g(xi): return (xi/L)**2.*(1.-xi/L)
+def un(xi, an, bn, n, L, ti):
+    return np.sin(n*np.pi*xi/L)*(an*np.cos(n*np.pi*c*ti/L)+bn*np.sin(n*np.pi*c*ti/L))
+
+#calculate the first nmax coefficients an and bn
+for ni in np.arange(1, nmax+1) :
+    a[ni-1]=2.0/L*integrate.quad(lambda xi: f(xi)*np.sin(ni*np.pi*xi/L),0,L)[0]
+    b[ni-1]=2.0/(ni*np.pi*c)*integrate.quad(lambda xi: g(xi)*np.sin(ni*np.pi*xi/L),0,L)[0]
+
+for i in np.arange(0 ,nx):
+    for j in np.arange (0,nt) :
+        uu = 0.0
+        for ni in np.arange (1, nmax+1):
+            uu = uu + un(x[i],a[ni-1],b[ni-1],ni,L,t[j])
+        u[i,j] = uu
+
+x,t = np.meshgrid(x,t)
+
+fig = plt.figure()
+ax = fig.add_subplot (projection= '3d')
+surf = ax.plot_surface(x, t, u.T, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Wave Equation')
+plt.show()
---
+++ 09/Task_1_7.py
@@ -0,0 +1,5 @@
+from Task_1_5 import Task_
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+import scipy. integrate as integrate
